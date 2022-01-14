5fc56dfc2eca02e4caecd9d48e69f24c4e2404c7
Se7en <455732253@qq.com>
2022-01-15 01:03:15 +0800

    Update log4j-scan.py

--- log4j-scan.py
+++ log4j-scan.py
@@ -12,18 +12,9 @@ import argparse
 import random
 import threading
 from queue import Queue
-
 import requests
-import time
 import sys
 from urllib import parse as urlparse
-import base64
-import json
-from uuid import uuid4
-from base64 import b64encode
-from Crypto.Cipher import AES, PKCS1_OAEP
-from Crypto.PublicKey import RSA
-from Crypto.Hash import SHA256
 from termcolor import cprint
 
 
@@ -107,12 +98,6 @@ parser.add_argument("--exclude-user-agent-fuzzing",
                     dest="exclude_user_agent_fuzzing",
                     help="Exclude User-Agent header from fuzzing - useful to bypass weak checks on User-Agents.",
                     action='store_true')
-parser.add_argument("--wait-time",
-                    dest="wait_time",
-                    help="Wait time after all URLs are processed (in seconds) - [Default: 5].",
-                    default=5,
-                    type=int,
-                    action='store')
 parser.add_argument("--waf-bypass",
                     dest="waf_bypass_payloads",
                     help="Extend scans with WAF bypass payloads.",
@@ -124,11 +109,6 @@ parser.add_argument("--test-CVE-2021-45046",
                     dest="cve_2021_45046",
                     help="Test using payloads for CVE-2021-45046 (detection payloads).",
                     action='store_true')
-parser.add_argument("--dns-callback-provider",
-                    dest="dns_callback_provider",
-                    help="DNS Callback provider (Options: dnslog.cn, interact.sh) - [Default: interact.sh].",
-                    default="interact.sh",
-                    action='store')
 parser.add_argument("--custom-dns-callback-host",
                     dest="custom_dns_callback_host",
                     help="Custom DNS Callback Host.",
@@ -148,7 +128,6 @@ if args.proxy:
 
 if args.custom_waf_bypass_payload:
     waf_bypass_payloads.append(args.custom_waf_bypass_payload)
-
 
 def get_fuzzing_headers(payload):
     fuzzing_headers = {}
@@ -190,88 +169,6 @@ def get_cve_2021_45046_payloads(callback_host, random_string):
         new_payload = new_payload.replace("{{random}}", random_string)
         payloads.append(new_payload)
     return payloads
-
-
-class Dnslog(object):
-    def __init__(self):
-        self.s = requests.session()
-        req = self.s.get("http://www.dnslog.cn/getdomain.php",
-                         proxies=proxies,
-                         timeout=30)
-        self.domain = req.text
-
-    def pull_logs(self):
-        req = self.s.get("http://www.dnslog.cn/getrecords.php",
-                         proxies=proxies,
-                         timeout=30)
-        return req.json()
-
-
-class Interactsh:
-    # Source: https://github.com/knownsec/pocsuite3/blob/master/pocsuite3/modules/interactsh/__init__.py
-    def __init__(self, token="", server=""):
-        rsa = RSA.generate(2048)
-        self.public_key = rsa.publickey().exportKey()
-        self.private_key = rsa.exportKey()
-        self.token = token
-        self.server = server.lstrip('.') or 'interact.sh'
-        self.headers = {
-            "Content-Type": "application/json",
-        }
-        if self.token:
-            self.headers['Authorization'] = self.token
-        self.secret = str(uuid4())
-        self.encoded = b64encode(self.public_key).decode("utf8")
-        guid = uuid4().hex.ljust(33, 'a')
-        guid = ''.join(i if i.isdigit() else chr(ord(i) + random.randint(0, 20)) for i in guid)
-        self.domain = f'{guid}.{self.server}'
-        self.correlation_id = self.domain[:20]
-
-        self.session = requests.session()
-        self.session.headers = self.headers
-        self.session.verify = False
-        self.session.proxies = proxies
-        self.register()
-
-    def register(self):
-        data = {
-            "public-key": self.encoded,
-            "secret-key": self.secret,
-            "correlation-id": self.correlation_id
-        }
-        res = self.session.post(
-            f"https://{self.server}/register", headers=self.headers, json=data, timeout=30)
-        if 'success' not in res.text:
-            raise Exception("Can not initiate interact.sh DNS callback client")
-
-    def pull_logs(self):
-        result = []
-        url = f"https://{self.server}/poll?id={self.correlation_id}&secret={self.secret}"
-        res = self.session.get(url, headers=self.headers, timeout=30).json()
-        aes_key, data_list = res['aes_key'], res['data']
-        for i in data_list:
-            decrypt_data = self.__decrypt_data(aes_key, i)
-            result.append(self.__parse_log(decrypt_data))
-        return result
-
-    def __decrypt_data(self, aes_key, data):
-        private_key = RSA.importKey(self.private_key)
-        cipher = PKCS1_OAEP.new(private_key, hashAlgo=SHA256)
-        aes_plain_key = cipher.decrypt(base64.b64decode(aes_key))
-        decode = base64.b64decode(data)
-        bs = AES.block_size
-        iv = decode[:bs]
-        cryptor = AES.new(key=aes_plain_key, mode=AES.MODE_CFB, IV=iv, segment_size=128)
-        plain_text = cryptor.decrypt(decode)
-        return json.loads(plain_text[16:])
-
-    def __parse_log(self, log_entry):
-        new_log_entry = {"timestamp": log_entry["timestamp"],
-                         "host": f'{log_entry["full-id"]}.{self.domain}',
-                         "remote_address": log_entry["remote-address"]
-                         }
-        return new_log_entry
-
 
 def parse_url(url):
     """
@@ -359,7 +256,6 @@ def scan_url():
                     cprint(f"EXCEPTION: {e}")
         q.task_done()
 
-
 def main():
     urls = []
     if args.url:
@@ -376,17 +272,7 @@ def main():
     if args.custom_dns_callback_host:
         cprint(f"[•] Using custom DNS Callback host [{args.custom_dns_callback_host}]. No verification will be done after sending fuzz requests.")
         dns_callback_host = args.custom_dns_callback_host
-    '''
-    else:
-        cprint(f"[•] Initiating DNS callback server ({args.dns_callback_provider}).")
-        if args.dns_callback_provider == "interact.sh":
-            dns_callback = Interactsh()
-        elif args.dns_callback_provider == "dnslog.cn":
-            dns_callback = Dnslog()
-        else:
-            raise ValueError("Invalid DNS Callback provider")
-        dns_callback_host = dns_callback.domain
-    '''
+
     cprint("[%] Checking for Log4j RCE CVE-2021-44228.", "magenta")
     global q
     q = Queue()
@@ -405,18 +291,6 @@ def main():
     if args.custom_dns_callback_host:
         cprint("[•] Payloads sent to all URLs. Custom DNS Callback host is provided, please check your logs to verify the existence of the vulnerability. Exiting.", "cyan")
         return
-
-    cprint("[•] Payloads sent to all URLs. Waiting for DNS OOB callbacks.", "cyan")
-    cprint("[•] Waiting...", "cyan")
-    time.sleep(int(args.wait_time))
-    records = dns_callback.pull_logs()
-    if len(records) == 0:
-        cprint("[•] Targets do not seem to be vulnerable.", "green")
-    else:
-        cprint("[!!!] Targets Affected", "yellow")
-        for i in records:
-            cprint(json.dumps(i), "yellow")
-
 
 if __name__ == "__main__":
     try:
